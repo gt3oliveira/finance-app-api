64c1c5b699e71ac61c75258fb6b631405d1efb1c
gt3oliveira <gt3oliveira@gmail.com>
2025-08-19 14:37:04 -0300

    fix: update version 9 eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,18 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import { defineConfig } from 'eslint/config'
 
-export default defineConfig([
+export default [
+    js.configs.recommended,
     {
-        files: ['**/*.{js,mjs,cjs}'],
-        plugins: { js },
-        extends: ['js/recommended'],
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+                ...globals.jest,
+            },
+        },
     },
-    {
-        files: ['**/*.{js,mjs,cjs}'],
-        languageOptions: { globals: globals.node },
-    },
-])
+]
