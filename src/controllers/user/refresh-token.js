21554c08c5ebb2ba402ea01c9aa681b1998a1217
gt3oliveira <gt3oliveira@gmail.com>
2025-08-22 18:23:36 -0300

    feat: add RefreshTokenController

---
+++ src/controllers/user/refresh-token.js
@@ -0,0 +1,36 @@
+import { badRequest, ok, serverError, unauthorized } from '../helpers/index.js'
+import { UnauthorizedError } from '../../errors/index.js'
+import { refreshTokenSchema } from '../../schemas/user.js'
+import { ZodError } from 'zod'
+export class RefreshTokenController {
+    constructor(refreshTokenUseCase) {
+        this.refreshTokenUseCase = refreshTokenUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body
+
+            await refreshTokenSchema.parseAsync(params)
+
+            const response = this.refreshTokenUseCase.execute(
+                params.refreshToken,
+            )
+
+            return ok(response)
+        } catch (error) {
+            console.log('<< Error in RefreshTokenUseCase >>', error)
+
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.issues[0].message,
+                })
+            }
+
+            if (error instanceof UnauthorizedError) {
+                return unauthorized()
+            }
+            return serverError()
+        }
+    }
+}
