e961adc7b0723c1ab21606688ccf68e9547be2e5
gt3oliveira <gt3oliveira@gmail.com>
2025-08-22 17:44:38 -0300

    feat: add RefreshTokenUseCase

---
+++ src/use-cases/user/refresh-token.js
@@ -0,0 +1,26 @@
+import { UnauthorizedError } from '../../errors'
+
+export class RefreshTokenUseCase {
+    constructor(tokensGeneratorAdapter, tokenVerifierAdapter) {
+        this.tokensGeneratorAdapter = tokensGeneratorAdapter
+        this.tokenVerifierAdapter = tokenVerifierAdapter
+    }
+
+    execute(refreshToken) {
+        try {
+            const decodedToken = this.tokenVerifierAdapter.execute(
+                refreshToken,
+                process.env.JWT_REFRESH_TOKEN_SECRET,
+            )
+
+            if (!decodedToken) {
+                throw new UnauthorizedError()
+            }
+
+            return this.tokensGeneratorAdapter.execute(decodedToken.userId)
+        } catch (error) {
+            console.log('<< Error in RefreshTokenUseCase >>', error)
+            throw new UnauthorizedError()
+        }
+    }
+}
