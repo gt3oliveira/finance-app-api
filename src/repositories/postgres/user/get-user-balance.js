8f19f1c0813be779e643cef80936ef4320eb3f35
gt3oliveira <gt3oliveira@gmail.com>
2025-08-27 16:30:36 -0300

    feat: add from and to params to date filtering to PostgresGetUserBalanceRepository

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -1,26 +1,45 @@
-import { Prisma } from '@prisma/client'
+import { Prisma, TransactionType } from '@prisma/client'
 import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetUserBalanceRepository {
-    async execute(userId) {
+    async execute(userId, from, to) {
+        const dateFilter = {
+            date: {
+                gte: new Date(from),
+                lte: new Date(to),
+            },
+        }
+
         const {
             _sum: { amount: _totalExpenses },
         } = await prisma.transaction.aggregate({
-            where: { user_id: userId, type: 'EXPENSE' },
+            where: {
+                user_id: userId,
+                type: TransactionType.EXPENSE,
+                ...dateFilter,
+            },
             _sum: { amount: true },
         })
 
         const {
             _sum: { amount: _totalEarnings },
         } = await prisma.transaction.aggregate({
-            where: { user_id: userId, type: 'EARNING' },
+            where: {
+                user_id: userId,
+                type: TransactionType.EARNING,
+                ...dateFilter,
+            },
             _sum: { amount: true },
         })
 
         const {
             _sum: { amount: _totalInvestments },
         } = await prisma.transaction.aggregate({
-            where: { user_id: userId, type: 'INVESTMENT' },
+            where: {
+                user_id: userId,
+                type: TransactionType.INVESTMENT,
+                ...dateFilter,
+            },
             _sum: { amount: true },
         })
 
