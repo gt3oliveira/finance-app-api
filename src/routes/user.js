ddb0bbd827900994566f0cf6c060e62bfbb753c6
gt3oliveira <gt3oliveira@gmail.com>
2025-08-27 14:04:27 -0300

    feat: ensure /balance route add query params from and to

--- src/routes/user.js
+++ src/routes/user.js
@@ -34,6 +34,10 @@ userRouter.get('/balance', auth, async (req, res) => {
         params: {
             userId: req.userId,
         },
+        query: {
+            from: req.query.from,
+            to: req.query.to,
+        },
     })
 
     res.status(statusCode).send(body)
